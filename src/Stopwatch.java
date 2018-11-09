2b2060ced505cccdf227b2d5a7414d588a3ea5bf
alxndrx <alexander@my-e-mail.net>
2018-11-09 17:00:49 +0100

    Improve Stopwatch

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -1,30 +1,42 @@
 public class Stopwatch
 {
     private long startTime, stopTime;
+    private boolean isRunning = false;
 
     public static Stopwatch startNew()
     {
-        return new Stopwatch();
-    }
-
-    public Stopwatch()
-    {
-        start();
+        var watch = new Stopwatch();
+        watch.start();
+        return watch;
     }
 
     public void start()
     {
         startTime = System.currentTimeMillis();
         stopTime = startTime;
+        isRunning = true;
     }
 
     public void stop()
     {
         stopTime = System.currentTimeMillis();
+        isRunning = false;
+    }
+
+    public boolean isRunning()
+    {
+        return isRunning;
     }
 
     public long millis()
     {
-        return stopTime - startTime;
+        if (!isRunning)
+        {
+            return stopTime - startTime;
+        }
+        else
+        {
+            return System.currentTimeMillis() - startTime;
+        }
     }
 }
